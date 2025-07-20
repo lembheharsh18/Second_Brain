741af94e40822122c4961c70628446005714b3c2
Harsh Lembhe <lembheharsh0508@gmail.com>
2025-07-20 20:38:24 +0530

    sign-in and out endpoint

---
+++ src/db.ts
@@ -0,0 +1,9 @@
+import mongoose,{model, Model, Schema} from 'mongoose';
+mongoose.connect("mongodb://localhost:27017/brainly")
+
+const UserSchema = new Schema({
+    username: {type: String, unique:true},
+    password: String
+})
+
+export const UserModel = model("User", UserSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,66 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
+import { UserModel } from './db';
 
 const app = express();
-app.post("/api/v1/signin", (req,res) => {
-    
-})
-app.post("/api/v1/signup", (req,res) => {
+app.use(express.json());
+const JWT_PASSWORD="1212111";
 
-})
+app.post("/api/v1/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        });
+
+        res.json({
+            message: "User Signed Up"
+        });
+    } catch (e) {
+        res.status(411).json({
+            message: "User already exists"
+        });
+        
+    }
+});
+
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const existingUser = await UserModel.findOne({ username, password });
+
+        if (existingUser) {
+            const token = jwt.sign(
+                { id: existingUser._id },
+                process.env.JWT_PASSWORD || "your_jwt_secret"
+            );
+
+            res.json({ token });
+        } else {
+            res.status(403).json({
+                message: "Invalid Credentials"
+            });
+        }
+    } catch (e) {
+        if (e instanceof Error) {
+            res.status(500).json({
+                message: "Something went wrong",
+                error: e.message
+            });
+        } else {
+            res.status(500).json({
+                message: "Unknown error"
+            });
+        }
+    }
+});
+
 app.post("/api/v1/content", (req,res) => {
 
 })
@@ -18,3 +70,5 @@ app.get("/api/v1/content", (req,res) => {
 app.delete("/api/v1/content", (req,res) => {
 
 })
+
+app.listen(3000);
